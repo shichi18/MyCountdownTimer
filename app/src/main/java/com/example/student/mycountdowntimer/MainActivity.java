f9f6bdc2719b59f79dc39ee169c310b6b6bd4e72
kumaneko <n_shichi4279@outlook.jp>
2018-11-30 22:34:22 +0900

    [refactoring] Activity内にあったクラスを別クラスとして分けた
    別で余分なものが更新されてる（主に更新のやつ）

--- app/src/main/java/com/example/student/mycountdowntimer/MainActivity.java
+++ app/src/main/java/com/example/student/mycountdowntimer/MainActivity.java
@@ -1,38 +1,19 @@
 package com.example.student.mycountdowntimer;
 
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+/**
+ * カウントダウンタイマー
+ */
+
 public class MainActivity extends AppCompatActivity {
 
-    TextView mTimerText;
     MyCountDownTimer mTimer;
     FloatingActionButton mFab;
-
-    public class MyCountDownTimer extends CountDownTimer{
-        public boolean isRunning =false;
-
-        public MyCountDownTimer (long millisInFuture, long countDownInterval){
-            super(millisInFuture,countDownInterval);
-        }
-
-        @Override
-        public void onTick(long millisUntilFinished) {
-            long minute =millisUntilFinished/1000/60;
-            long second=millisUntilFinished/1000%60;
-            mTimerText.setText(String.format("%1d:%2$02d",minute,second));
-        }
-
-        @Override
-        public void onFinish() {
-            mTimerText.setText("0:00");
-        }
-    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,28 +21,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        mTimerText =(TextView) findViewById(R.id.timer_text);
-        mTimerText.setText("3:00");
-        mTimer = new MyCountDownTimer(3 * 60 * 1000,100);
+        mTimer = new MyCountDownTimer(3 * 60 * 1000, 100);
 
-        mFab = (FloatingActionButton)findViewById(R.id.play_stop);
-        mFab.setOnClickListener(new View.OnClickListener(){
+        mTimer.mTimerText = (TextView) findViewById(R.id.timer_text);
+        mTimer.mTimerText.setText("3:00");
+        mFab = (FloatingActionButton) findViewById(R.id.play_stop);
 
+        //ボタンクリック
+        mFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mTimer.isRunning){
-                    mTimer.isRunning =false;
+                if (mTimer.isRunning) {
+                    mTimer.isRunning = false;
                     mTimer.cancel();
                     mFab.setImageResource(R.drawable.ic_play_arrow_black_24dp);
-
-                }else{
-                    mTimer.isRunning=true;
+                } else {
+                    mTimer.isRunning = true;
                     mTimer.start();
                     mFab.setImageResource(R.drawable.ic_stop_black_24dp);
                 }
             }
         });
-
 
 
     }
