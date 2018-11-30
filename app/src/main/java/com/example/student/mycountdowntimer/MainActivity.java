ea8805f4d547a3e94c5e460c6d3fac2997aea6bb
kumaneko <n_shichi4279@outlook.jp>
2018-12-01 03:08:57 +0900

    [add + change] Fragment上に移行できた

--- app/src/main/java/com/example/student/mycountdowntimer/MainActivity.java
+++ app/src/main/java/com/example/student/mycountdowntimer/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         //Create CountDownTimerFragment
         CountDownTimerFragment countDownTimerFragment = new CountDownTimerFragment();
         getSupportFragmentManager().beginTransaction().replace(R.id.change_layout, countDownTimerFragment).commit();
